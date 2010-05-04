9600a5b06570ed0379b7e8dd8dc16c6e7dbe0d92
Briam <brian.fro@gmail.com>
2010-05-04 10:26:27 +0200

    gui

--- gui/MainFrame.java
+++ gui/MainFrame.java
@@ -2,7 +2,15 @@ package gui;
 
 import javax.swing.JFrame;
 
-public class MainFrame extends JFrame{
-		
+public class MainFrame extends JFrame {
+	
+	public MainFrame() {
 
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("asd v0.8");
+		this.setLayout(null);
+		this.setResizable(false);
+		this.setSize(362,312);
+		
+	}
 }
