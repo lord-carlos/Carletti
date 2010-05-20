e2b7a1ef4cdd3150db26d8bd89e77e4e7f15ce67
Briam <brian.fro@gmail.com>
2010-05-20 12:17:53 +0200

    p

--- model/ProductType.java
+++ model/ProductType.java
@@ -14,65 +14,65 @@ import com.sun.image.codec.jpeg.JPEGImageEncoder;
  */
 
 
-ublic class ProductType {
-    private String name;
-    private ProcessLine processLine = null;
-    private ImageIcon picture = null;
-    private ArrayList<IntermediateProduct> intermediateProducts  = new ArrayList<IntermediateProduct>();
+public class ProductType {
+	private String name;
+	private ProcessLine processLine = null;
+	private ImageIcon picture = null;
+	private ArrayList<IntermediateProduct> intermediateProducts  = new ArrayList<IntermediateProduct>();
 
-    public ProductType(String name){
-    	this.setName(name);
-    }
+	public ProductType(String name){
+		this.setName(name);
+	}
 
-    public String getName(){
-        return this.name;
-    }
+	public String getName(){
+		return this.name;
+	}
 
-    public void setName(String name){
-    	this.name=name;
-    }
+	public void setName(String name){
+		this.name=name;
+	}
 
-    public ProcessLine getProcessLine(){
-        return this.processLine;
-    }
-    
-    public ImageIcon getPicture() {
+	public ProcessLine getProcessLine(){
+		return this.processLine;
+	}
+
+	public ImageIcon getPicture() {
 		return picture;
 	}
 
-    /**
-     * not recomended to call this method
-     * @param processLine
-     */
-    public void setProcessLine(ProcessLine processLine){
-    		this.processLine=processLine;
-    }
+	/**
+	 * not recomended to call this method
+	 * @param processLine
+	 */
+	public void setProcessLine(ProcessLine processLine){
+		this.processLine=processLine;
+	}
 
-    public ArrayList<IntermediateProduct> getIntermediateProducts(){
-        return this.intermediateProducts;
-    }
-    
-    public void setPicture(ImageIcon picture) {
+	public ArrayList<IntermediateProduct> getIntermediateProducts(){
+		return this.intermediateProducts;
+	}
+
+	public void setPicture(ImageIcon picture) {
 		this.picture = picture;
 	}
 
-    public void addIntermediateProduct(IntermediateProduct intermediateProduct){
-    	this.intermediateProducts.add(intermediateProduct);
-    	if (intermediateProduct.getProductType()!=this){
-    		intermediateProduct.setProductType(this);
-    	}
-    }
+	public void addIntermediateProduct(IntermediateProduct intermediateProduct){
+		this.intermediateProducts.add(intermediateProduct);
+		if (intermediateProduct.getProductType()!=this){
+			intermediateProduct.setProductType(this);
+		}
+	}
 
-    /**
-     * always call this method trougth setProductType method in IntermediateProduct
-     * @param intermediateProduct
-     */
-    public void removeIntermediateProduct(IntermediateProduct intermediateProduct){
-    	this.intermediateProducts.remove(intermediateProduct);
-    }
-    
-    public String toString() {
-    	return this.name;
-    }
+	/**
+	 * always call this method trougth setProductType method in IntermediateProduct
+	 * @param intermediateProduct
+	 */
+	public void removeIntermediateProduct(IntermediateProduct intermediateProduct){
+		this.intermediateProducts.remove(intermediateProduct);
+	}
+
+	public String toString() {
+		return this.name;
+	}
 
 }
